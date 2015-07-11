5406dbf1294fb99f8a25cf892986a3efd97afe58
aleksandr <aatarasoff@gmail.com>
2015-07-11 18:18:58 +0300

    add exception throw to auth token exchanger interface

--- src/main/java/ru/aatarasoff/thrift/api/gateway/core/AuthTokenExchanger.java
+++ src/main/java/ru/aatarasoff/thrift/api/gateway/core/AuthTokenExchanger.java
@@ -1,6 +1,7 @@
 package ru.aatarasoff.thrift.api.gateway.core;
 
 import org.apache.thrift.TBase;
+import org.apache.thrift.TException;
 
 /**
  * Created by aleksandr on 09.07.15.
@@ -8,5 +9,5 @@ import org.apache.thrift.TBase;
 public interface AuthTokenExchanger<T extends TBase, U extends  TBase> {
     T createEmptyAuthToken();
 
-    U process(T authToken);
+    U process(T authToken) throws TException;
 }
